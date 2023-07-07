9b4163740936851e3f508d34998209add68d573c
SantiagoCortes <santiagocortes3107@outlook.com>
2023-07-06 23:39:12 -0300

    Preentrega2Cortes

---
+++ distrinortecommerce/src/components/itemDetails.jsx
@@ -0,0 +1,9 @@
+export const ItemDetail = ({car}) => {
+return(<>
+    <div>{car.car_factory}</div>
+    <div>{car.car_factory}</div>
+    <div>{car.car_factory}</div>
+    <div>{car.car_factory}</div>
+    <img src={car.img} alt ={car.car_model} /></>
+)
+}
